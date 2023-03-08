20a28515125d1182008f871af9c69780f5d3570f
nihatcan <nihatesatcan17@gmail.com>
2023-03-08 18:13:58 +0000

    added rancher setup files

---
+++ infrastructure/msp-23/variable.tf
@@ -0,0 +1,13 @@
+variable "region" {}
+variable "mykey" {}
+variable "tags" {}
+variable "myami" {
+  description = "ubuntu 20.04 ami"
+}
+variable "instancetype" {}
+variable "secgrname" {}
+variable "domain-name" {}
+variable "rancher-subnet" {}
+variable "hostedzone" {}
+variable "vpc-id" {}
+variable "alb-tag" {}
